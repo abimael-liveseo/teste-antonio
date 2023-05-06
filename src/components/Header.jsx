e2fd322c3e664b7d239de3d7e0de1ed37202f407
Neto Flavio <95658733+zNetinho@users.noreply.github.com>
2023-05-06 12:01:58 -0300

    Criado o Header do projeto

---
+++ src/components/Header.jsx
@@ -0,0 +1,51 @@
+/* eslint-disable no-unused-vars */
+import logo from "../../public/img/logo.svg";
+import "../styles/index.css";
+import Menu from "./Menu/Menu";
+
+function Header() {
+  return (
+    <div className="bg-white w-full flex justify-between rounded-lg">
+      <div className="flex">
+        <div className="my-5 mx-10">
+          <img src={logo} alt="" className="logo w-52 h-12" />
+        </div>
+      </div>
+      <div className="menu m-auto text-xl">
+        <div className="mx-6">
+          <ul className="flex flex-row gap-10 font-medium">
+            <li>
+              <a href="#" className="hover:text-orange-400">Home</a>
+            </li>
+            <li>
+              <a href="#" className="hover:text-orange-400">About</a>
+            </li>
+            <li>
+              <a href="#" className="hover:text-orange-400">Service</a>
+              <ul>
+                <li className="flex relative top-11 justify-center items-center bg-orange-500 bg-gradient-to-r from-orange-300 rounded-md border-transparent ">
+                    <a href="#" className="text-white py-1 px-3">
+                        New
+                    </a>
+                </li>
+              </ul>
+            </li>
+            <li>
+              <a href="#" className="hover:text-orange-400">Team</a>
+            </li>
+            <li>
+              <a href="#" className="hover:text-orange-400">Contact</a>
+            </li>
+          </ul>
+        </div>
+      </div>
+      <div className="btn-register h-12 flex justify-end bg-orange-500 bg-gradient-to-r from-orange-300 rounded-lg border-2">
+        <div className="w-44 flex justify-center items-center text-center">
+          <button className="text-lg font-semibold">Get A Quote</button>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Header;
